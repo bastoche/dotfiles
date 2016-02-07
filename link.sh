59eb11ddd77b71946bca5113371ef05e71904e51
Bastien Duret <bastien.duret@gmail.com>
2016-02-07 23:03:54 +0100

    Add initial files

---
+++ link.sh
@@ -0,0 +1,9 @@
+DOTFILES=${PWD}
+
+# TODO improve this
+
+ln -sfv $DOTFILES/.aliases ~/.aliases
+ln -sfv $DOTFILES/.bash_profile ~/.bash_profile
+ln -sfv $DOTFILES/.exports ~/.exports
+ln -sfv $DOTFILES/.functions ~/.functions
+ln -sfv $DOTFILES/.inputrc ~/.inputrc
