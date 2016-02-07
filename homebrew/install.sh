d9c4a82949e35ce2bca8c299c35e735d289feb41
Bastien Duret <bastien.duret@gmail.com>
2016-02-07 17:21:51 +0100

    First commit

---
+++ homebrew/install.sh
@@ -0,0 +1,7 @@
+if [[ ! "$(type -P brew)" ]]; then
+	echo "Installing Homebrew"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "Installing Homebrew packages"
+brew bundle
